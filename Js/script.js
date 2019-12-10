1e6b9be55a760f70dd0b70d33e021ef25c9a3938
OsaidAyadi <osaid.step@gmail.com>
2019-12-10 17:10:33 +0200

    Production the chat project .

--- Js/script.js
+++ Js/script.js
@@ -1,22 +1,27 @@
 document.addEventListener("DOMContentLoaded", function(){
-const friendList = document.getElementsByClassName('friend-side')[0];
 const userInput = document.getElementsByClassName('user-input')[0];
 const sendMsg = document.getElementsByClassName('chat-btn-send')[0];
 const chatBody = document.getElementsByClassName('chat-body')[0];
 const viewProfile = document.getElementsByClassName('chat-left-side')[0];
 const hideProfile = document.getElementsByClassName('profile-exit')[0];
 const profile = document.getElementsByClassName('profile-side')[0];
+const chatRow = document.getElementsByClassName('active-chat')[0];
+const chatSide = document.getElementsByClassName('chat-side')[0];
+const friendSide = document.getElementsByClassName('friend-side')[0];
+const pageHeader = document.getElementsByClassName('header')[0];
+const sideBar = document.getElementsByClassName('sidebar')[0];
 
-    friendList.addEventListener('mouseover',()=>{
-        friendList.classList.add('scroll');
-    })
+    //This function is for open chat side only !
+    function showChatSide() {
+        if (window.innerWidth <= 990){
+        friendSide.classList.add('hide');
+        sideBar.classList.add('hide');
+        pageHeader.classList.add('hide');
+        chatSide.classList.add('active');
+    }
+    }
 
-    friendList.addEventListener('mouseleave',()=>{
-        setTimeout(() => {
-            friendList.classList.remove('scroll');
-        }, 1000);
-    })
-
+    //This eventListener for add message to Chat-Body .
     sendMsg.addEventListener('click',()=>{
         let msgDiv = document.createElement("div");
         msgDiv.classList.add("message-send");
@@ -25,14 +30,36 @@ const profile = document.getElementsByClassName('profile-side')[0];
         msgDiv.appendChild(paragraph);
         chatBody.appendChild(msgDiv);
         chatBody.scrollTo(0,chatBody.scrollHeight);
+        userInput.value='';
     })
 
-    viewProfile.addEventListener('click',()=>{
+    //View an hide profile in mobile case and view user profile in desktop screen size case.
+    viewProfile.addEventListener('click',(e)=>{
         profile.classList.add('active');
+        if (viewProfile.offsetWidth>230){
+        if(e.clientX <= 23) {
+            chatSide.classList.remove('active');
+            friendSide.classList.remove('hide');
+            pageHeader.classList.remove('hide');
+        } else { 
+            chatSide.classList.remove('active');
+            profile.classList.add('open');
+        }
+        hideProfile.addEventListener('click',()=>{
+            profile.classList.remove('open');
+            showChatSide();
+        })
+    }
     })
+
+    //Hide user profile in desktop screen size case.
     hideProfile.addEventListener('click',()=>{
         profile.classList.remove('active');
     })
 
+    //For open chat side full width .
+    chatRow.addEventListener('click',()=>{
+        showChatSide();
+    })
 
 });
